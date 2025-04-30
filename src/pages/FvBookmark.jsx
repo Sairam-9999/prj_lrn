4de5a8f428dac994af1619389cb837a73d86f986
sairam-9999 <sairam4916@gmail.com>
2025-04-30 12:20:28 -0500

    Round 3

---
+++ src/pages/FvBookmark.jsx
@@ -0,0 +1,13 @@
+import '../CSS/Favorites.css';
+
+function FvBookmark(){
+    return(
+        <div className="fav-bm">
+            <h2>No favorite movies yet</h2>
+            <p>Start adding to this list</p>
+        </div>
+
+    )
+}
+
+export default FvBookmark;
